0aafbfd19e043b68ca28828b0899ac81e44718ec
Tomas Garay <tomasgaray07@gmail.com>
2019-12-10 20:05:04 -0600

    Commit Inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { AngularFireAuth} from '@angular/fire/auth'
+import {  User } from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mastergym';
+  usuario: User;
+  cargando: boolean = true;
+  constructor(private afAuth: AngularFireAuth)
+  {
+
+  
+    this.afAuth.auth.createUserWithEmailAndPassword
+    this.afAuth.user.subscribe((usuario)=>{
+
+      this.cargando = false;
+      this.usuario = usuario;
+    });
+  }
+ 
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD3OqibMcrtl7N1N3Cextc4-5u0snny208",
+    authDomain: "mastergym-42697.firebaseapp.com",
+    databaseURL: "https://mastergym-42697.firebaseio.com",
+    projectId: "mastergym-42697",
+    storageBucket: "mastergym-42697.appspot.com",
+    messagingSenderId: "170792895827",
+    appId: "1:170792895827:web:2ee5818aafc1d331"
+  }
 };
 
 /*
